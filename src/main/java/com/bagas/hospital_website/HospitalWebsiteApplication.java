a59884692fdeac16e1cc337d99a903b8954b8eff
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-09 18:27:01 +0300

    initial commit

---
+++ src/main/java/com/bagas/hospital_website/HospitalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.bagas.hospital_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagas/hospital_website/HospitalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagas.hospital_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
