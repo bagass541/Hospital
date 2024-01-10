d402e9e1a272dea3298e496bda7dd222aed77467
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-10 20:29:11 +0300

    make home page

---
+++ src/main/java/com/bagas/hospital_website/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package com.bagas.hospital_website.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+	
+	@GetMapping
+	public String showHomePage() {
+		return "home";
+	}
+	
+	@GetMapping("/services")
+	public String showServices() {
+		return "services";
+	}
+	
+	@GetMapping("/list-structure")
+	public String showListStructure() {
+		return "list-structure";
+	}
+	
+	@GetMapping("/about-us") 
+	public String showAboutUs() {
+		return "about-us";
+	}
+	
+	@GetMapping("/contacts")
+	public String showContacts() {
+		return "contacts";
+	}
+}
