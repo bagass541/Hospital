be421e3d7f17c2bea96c168803beba55c74526b4
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-20 13:53:07 +0300

    make a service for doctor model

---
+++ src/main/java/com/bagas/hospital_website/services/DoctorService.java
@@ -0,0 +1,30 @@
+package com.bagas.hospital_website.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bagas.hospital_website.models.Doctor;
+import com.bagas.hospital_website.models.DoctorType;
+import com.bagas.hospital_website.models.DoctorTypeConverter;
+import com.bagas.hospital_website.repositories.DoctorRepository;
+
+@Service
+public class DoctorService {
+
+	@Autowired
+	private DoctorRepository doctorRepo;
+	
+	@Autowired
+	private DoctorTypeConverter doctorTypeConverter;
+	
+	public List<Doctor> getAllDoctors() {
+		return doctorRepo.findAll();
+	}
+	
+	public List<Doctor> getDoctorsByType(String type) {
+		DoctorType doctorType = doctorTypeConverter.convertToEntityAttribute(type);
+		return doctorRepo.findByDoctorType(doctorType);
+	}
+}
