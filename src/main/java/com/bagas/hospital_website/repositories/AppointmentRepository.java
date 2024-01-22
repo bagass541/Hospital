5a4fe971824bd6d62bfd2f0f364d2cd07379e2ce
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-22 17:37:50 +0300

    add methods for dates, times and finding appointment by date

--- src/main/java/com/bagas/hospital_website/repositories/AppointmentRepository.java
+++ src/main/java/com/bagas/hospital_website/repositories/AppointmentRepository.java
@@ -1,9 +1,33 @@
 package com.bagas.hospital_website.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bagas.hospital_website.models.Appointment;
 
+
+import java.util.List;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
 
+	@Query(value = "SELECT * FROM appointments WHERE DATE(time) = :date LIMIT 1", nativeQuery = true)
+	Optional<Appointment> findAppointmentByDate(@Param("date") LocalDate date);
+	
+	@Query(value = "SELECT DISTINCT DATE(time) FROM appointments WHERE doctor_id = :doctorId AND user_id IS NULL AND DATE(time) BETWEEN :startDate AND :endDate",
+			nativeQuery = true)
+	List<Date> findAvailableDatesByDoctorDates(@Param("doctorId") long doctorId,
+									   				@Param("startDate") LocalDate startDate,
+									   				@Param("endDate") LocalDate endDate);
+	
+	@Query(value = "SELECT cast(time as time) FROM appointments WHERE doctor_id = :doctorId AND user_id IS NULL AND DATE(time) = :date",
+			nativeQuery = true)
+	List<Time> findAvailableTimeByDoctorDate(@Param("doctorId") long doctorId,
+												  @Param("date") LocalDate date);
 }
