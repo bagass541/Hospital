81c249d5fe486aacc647abd75c5018838470b493
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-24 21:31:58 +0300

    add new model and repository, service for it

---
+++ src/main/java/com/bagas/hospital_website/models/Procedure.java
@@ -0,0 +1,25 @@
+package com.bagas.hospital_website.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "procedures")
+@Entity
+@Data
+public class Procedure {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	
+	private int minutes;
+	
+	private int price;
+
+}
