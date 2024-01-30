feaf2738151a44c0bd32eab34865649ca8b90e8c
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-30 20:38:53 +0300

    add new methods for deleting, adding and finding users

--- src/main/java/com/bagas/hospital_website/repositories/UserRepository.java
+++ src/main/java/com/bagas/hospital_website/repositories/UserRepository.java
@@ -1,12 +1,18 @@
 package com.bagas.hospital_website.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bagas.hospital_website.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByUsername(String username);
+	
+	@Query("SELECT u FROM User u JOIN u.authorities a JOIN u.userInfo ui where a.authority = 'ROLE_USER' ORDER BY ui.fio")
+	List<User> findAllUsersOrderByFio();
+	
 }
--- src/main/java/com/bagas/hospital_website/services/UserService.java
+++ src/main/java/com/bagas/hospital_website/services/UserService.java
@@ -1,6 +1,9 @@
 package com.bagas.hospital_website.services;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -8,16 +11,27 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.bagas.hospital_website.models.Role;
 import com.bagas.hospital_website.models.User;
+import com.bagas.hospital_website.models.UserInfo;
 import com.bagas.hospital_website.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService implements UserDetailsService{
 
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleService roleService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -29,6 +43,10 @@ public class UserService implements UserDetailsService{
 		User user = userOptional.get();
 		
 		return user;
+	}
+	
+	public List<User> getAllUsersOrderByFio() {
+		return userRepo.findAllUsersOrderByFio();
 	}
 	
 	public String getFio() {
@@ -44,5 +62,42 @@ public class UserService implements UserDetailsService{
 	public User getCurrentUser() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return (User) authentication.getPrincipal();
+	}
+	
+	@Transactional
+	public void deleteUserById(long id) {
+		userRepo.deleteById(id);
+	}
+	
+	@Transactional
+	public void addUser(String username, String password, String fio, String number, Long roleId) {			
+		User user = createUser(username, password, fio, number, roleId);
+		userRepo.save(user);
+		
+	}
+	
+	private User createUser(String username, String password, String fio, String number, Long roleId) {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setFio(fio);
+		userInfo.setNumber(number);
+		
+		Role role = roleService.getById(roleId);
+		
+		Set<Role> roles = new HashSet<>();
+		roles.add(role);
+		
+		
+		User user = User.builder()
+				.username(username)
+				.password(passwordEncoder.encode(password))
+				.userInfo(userInfo)
+				.isAccountNonExpired(true)
+				.isAccountNonLocked(true)
+				.isCredentialsNonExpired(true)
+				.isEnabled(true)
+				.authorities(roles)
+				.build();
+	
+		return user;
 	}
 }
