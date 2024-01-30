b287d99dd03920ee0a8161a1f961521892ffa763
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-30 20:37:10 +0300

    add role repository

---
+++ src/main/java/com/bagas/hospital_website/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.bagas.hospital_website.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bagas.hospital_website.models.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
