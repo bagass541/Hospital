25cb5c5b0783ef6c57018491f875770aec50056d
Alexandr Bogoslovskiy <balics@mail.ru>
2024-01-30 20:37:29 +0300

    add role service

---
+++ src/main/java/com/bagas/hospital_website/services/RoleService.java
@@ -0,0 +1,25 @@
+package com.bagas.hospital_website.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bagas.hospital_website.models.Role;
+import com.bagas.hospital_website.repositories.RoleRepository;
+
+@Service
+public class RoleService {
+
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	public List<Role> getAllRoles() {
+		return roleRepo.findAll();
+	}
+	
+	public Role getById(Long id) {
+		return roleRepo.findById(id).get();
+	}
+}
+
